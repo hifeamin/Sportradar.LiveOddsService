b55d1588aeee3e9a9aab5190aa41fc74ef09ae6d
Amin <Amin@AMIN-PC>
2024-03-24 21:35:17 +0100

    Test: Write tests of get all match repository method

--- test/Sportradar.LiveOddsService.Data.InMemoeyCollection.Tests/MatchRepositoryTest.cs
+++ test/Sportradar.LiveOddsService.Data.InMemoeyCollection.Tests/MatchRepositoryTest.cs
@@ -44,5 +44,43 @@ namespace Sportradar.LiveOddsService.Data.InMemoeyCollection.Tests {
                 .And.ContainValue(match)
                 .And.ContainKey($"{match.HomeTeam}-{match.AwayTeam}");
         }
+
+        [Fact]
+        public async Task GetAllAsync_EmptyCollection_ShouldReturnEmptyCollection() {
+            // Arrange
+            DbContext dbContext = new();
+            IMatchRepository matchRepository = new MatchRepository(dbContext);
+
+            // Act
+            var result = await matchRepository.GetAllAsync();
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.BeEmpty();
+        }
+
+        [Fact]
+        public async Task GetAllAsync_WithData_ShouldReturnAllData() {
+            Match match1 = new() {
+                HomeTeam = "Home Team 1",
+                AwayTeam = "Away Team 1"
+            };
+            Match match2 = new() {
+                HomeTeam = "Home Team 2",
+                AwayTeam = "Away Team 2"
+            };
+            DbContext dbContext = new();
+            dbContext.Matches.Add($"{match1.HomeTeam}-{match1.AwayTeam}", match1);
+            dbContext.Matches.Add($"{match2.HomeTeam}-{match2.AwayTeam}", match2);
+            IMatchRepository matchRepository = new MatchRepository(dbContext);
+
+            // Act
+            var result = await matchRepository.GetAllAsync();
+
+            // Assert
+            result.Should().NotBeNull()
+                .And.HaveCount(2)
+                .And.AllBeEquivalentTo(new Match[] { match1, match2 });
+        }
     }
 }
