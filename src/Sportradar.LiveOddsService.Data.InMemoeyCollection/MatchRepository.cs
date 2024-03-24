c4c78f6f625feea59350e79467ff3494edf867fd
Amin <Amin@AMIN-PC>
2024-03-24 23:19:15 +0100

    Refactor: refactor generate item key

--- src/Sportradar.LiveOddsService.Data.InMemoeyCollection/MatchRepository.cs
+++ src/Sportradar.LiveOddsService.Data.InMemoeyCollection/MatchRepository.cs
@@ -10,7 +10,7 @@ namespace Sportradar.LiveOddsService.Data.InMemoeyCollection {
         public MatchRepository(DbContext dbContext) => _dbContext = dbContext;
 
         public Task AddAsync(Match match) {
-            _dbContext.Matches.Add($"{match.HomeTeam}-{match.AwayTeam}", match);
+            _dbContext.Matches.Add(GetItemKey(match), match);
             return Task.CompletedTask;
         }
 
@@ -18,15 +18,21 @@ namespace Sportradar.LiveOddsService.Data.InMemoeyCollection {
             Task.FromResult<IEnumerable<Match>>(_dbContext.Matches.Values);
 
         public Task<Match?> GetAsync(string homeTeam, string awayTeam) {
-            if(_dbContext.Matches.TryGetValue($"{homeTeam}-{awayTeam}", out Match data))
+            if(_dbContext.Matches.TryGetValue(GetItemKey(homeTeam, awayTeam), out Match data))
                 return Task.FromResult<Match?>(data);
             return Task.FromResult<Match?>(null);
         }
 
         public Task RemoveAsync(Match match) => throw new NotImplementedException();
         public Task UpdateAsync(Match match) {
-            _dbContext.Matches[$"{match.HomeTeam}-{match.AwayTeam}"] = match;
+            _dbContext.Matches[GetItemKey(match)] = match;
             return Task.CompletedTask;
         }
+
+        private string GetItemKey(string homeTeam, string awayTeam) =>
+            $"{homeTeam}-{awayTeam}";
+
+        private string GetItemKey(Match match) =>
+            GetItemKey(match.HomeTeam, match.AwayTeam);
     }
 }
